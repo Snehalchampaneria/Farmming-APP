864031488f31cae7d5f2ac0793ecc0639d1c593e
Snehalchampaneria <109916868+Snehalchampaneria@users.noreply.github.com>
2022-11-14 09:22:23 +0530

    Add files via upload

--- Single.js
+++ Single.js
@@ -22,7 +22,38 @@ export default function Single() {
         <div className='container '>
             <div className='row '>
                 <div className='col-md-6 mt-4 mb-4'>
-                    <Card>
+                    <div>
+                        <h1>{Product.title}</h1>
+                        <div className="list-group mt-1">
+                            <a href="#" className="list-group-item list-group-item-action"  aria-current="true">
+                                <div className="d-flex w-100 justify-content-between">
+                                    <h4 className="mb-1"  style={{color: "maroon"}}>wheat</h4>
+                                </div>
+                                <p className="mb-1">Some placeholder content in a paragraph.</p>
+                                <small>And some small print.</small>
+                            </a>             
+                        </div>
+                        <div className="list-group mt-1">
+                            <a href="#" className="list-group-item list-group-item-action"  aria-current="true">
+                                <div className="d-flex w-100 justify-content-between">
+                                    <h4 className="mb-1"  style={{color: "maroon"}}>Rice</h4>
+                                </div>
+                                <p className="mb-1">Some placeholder content in a paragraph.</p>
+                                <small>And some small print.</small>
+                            </a>             
+                        </div>
+                        <div className="list-group mt-1">
+                            <a href="#" className="list-group-item list-group-item-action"  aria-current="true">
+                                <div className="d-flex w-100 justify-content-between">
+                                    <h4 className="mb-1"  style={{color: "maroon"}}>Barley</h4>
+                                </div>
+                                <p className="mb-1">Some placeholder content in a paragraph.</p>
+                                <small>And some small print.</small>
+                            </a>             
+                        </div>
+                    </div>
+
+                    {/* <Card>
                         <Card.Body>
                             <Card.Title>{Product.title}</Card.Title>
                             <Card.Subtitle className='mb-2 text-muted'>
@@ -34,7 +65,7 @@ export default function Single() {
                             <Card.Link href='#'>card Link</Card.Link>
                             <Card.Link href='#'>onther link</Card.Link>
                         </Card.Body>
-                    </Card>
+                    </Card> */}
                 </div>
             </div>
         </div>
